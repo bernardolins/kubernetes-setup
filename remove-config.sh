b1fe2c603f20ecda36c5a23df972f2c5dfa39e02
Bernardo Lins <bernardolins28@gmail.com>
2015-12-10 23:58:45 -0200

    Create remove config script

---
+++ remove-config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+KUBE_DIRECTORY="/etc/kubernetes"
+SYSTEMD_DIRECTORY="/etc/systemd/system"
+ENVIRONMENT_FILE="kubernetes.env"
+
+SERVICE_FILES=("kube-api-server.service" "kube-controller-manager.service" "kube-proxy.service" "kubelet.service")
+
+if [ -e KUBE_DIRECTORY/ENVIRONMENT_FILE]; then
+  rm -s $(pwd)/kubelet.env KUBE_DIRECTORY
+fi
+
+for file in ${SERVICE_FILES[@]}; do
+  if [ -e SYSTEMD_DIRECTORY/file]; then
+    rm -rf SYSTEMD_DIRECTORY/file
+  fi
+done
