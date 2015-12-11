1643c78912fd3f6b20bb439b0a03e3b2cd8e6d9c
Bernardo Lins <bernardolins28@gmail.com>
2015-12-11 00:09:32 -0200

    Fix syntax error on string concatenation

--- remove-config.sh
+++ remove-config.sh
@@ -3,15 +3,17 @@
 KUBE_DIRECTORY="/etc/kubernetes"
 SYSTEMD_DIRECTORY="/etc/systemd/system"
 ENVIRONMENT_FILE="kubernetes.env"
+PWD=$(pwd)
 
-SERVICE_FILES=("kube-api-server.service" "kube-controller-manager.service" "kube-proxy.service" "kubelet.service")
+SERVICE_FILES=("kube-api-server.service" "kube-controller-manager.service" "kube-proxy.service" "kube-scheduler.service" "kubelet.service")
 
-if [ -e KUBE_DIRECTORY/ENVIRONMENT_FILE ]; then
-  rm -s $(pwd)/kubelet.env KUBE_DIRECTORY
+if [ -e "$KUBE_DIRECTORY/$ENVIRONMENT_FILE" ]; then
+  rm -rf "$KUBE_DIRECTORY/$ENVIRONMENT_FILE"
 fi
 
 for file in ${SERVICE_FILES[@]}; do
-  if [ -e SYSTEMD_DIRECTORY/file ]; then
-    rm -rf SYSTEMD_DIRECTORY/file
+  if [ -e "$SYSTEMD_DIRECTORY/$file" ]; then
+    rm -rf "$SYSTEMD_DIRECTORY/$file"
+    echo "$SYSTEMD_DIRECTORY/$file"
   fi
 done
