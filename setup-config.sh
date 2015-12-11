9bfa00acc65dfce7791ca2df334d26089384e9af
Bernardo Lins <bernardolins28@gmail.com>
2015-12-11 00:23:22 -0200

    Fix some syntax errors and add verification before create symlinks

--- setup-config.sh
+++ setup-config.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-$KUBE_DIRECTORY="/etc/kubernetes"
-$SYSTEMD_DIRECTORY="/etc/systemd/system"
+KUBE_DIRECTORY="/etc/kubernetes"
+SYSTEMD_DIRECTORY="/etc/systemd/system"
 
-if [ !-d $KUBE_DIRECTORY ]; then
+SERVICE_FILES=("kube-api-server.service" "kube-controller-manager.service" "kube-proxy.service" "kube-scheduler.service" "kubelet.service")
+
+
+if [ ! -d $KUBE_DIRECTORY ]; then
   mkdir -p $KUBE_DIRECTORY
 fi
 
-ln -s $(pwd)/kubelet.env $KUBE_DIRECTORY
-ln -s $(pwd)/kube-* $SYSTEMD_DIRECTORY
+ln -s "$(pwd)/kubelet.env" "$KUBE_DIRECTORY/kubelet.env"
+
+for file in ${SERVICE_FILES[@]}; do
+  if [ ! -e "$SYSTEMD_DIRECTORY/$file" ]; then
+    ln -s "$(pwd)/$file" $SYSTEMD_DIRECTORY
+  fi
+done
