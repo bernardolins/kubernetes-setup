7b85047c57d709228f4538f9f1b00514593a3afd
Bernardo Lins <bernardolins28@gmail.com>
2015-12-11 00:00:57 -0200

    Add exec permission and fix syntax error

--- remove-config.sh
+++ remove-config.sh
@@ -6,12 +6,12 @@ ENVIRONMENT_FILE="kubernetes.env"
 
 SERVICE_FILES=("kube-api-server.service" "kube-controller-manager.service" "kube-proxy.service" "kubelet.service")
 
-if [ -e KUBE_DIRECTORY/ENVIRONMENT_FILE]; then
+if [ -e KUBE_DIRECTORY/ENVIRONMENT_FILE ]; then
   rm -s $(pwd)/kubelet.env KUBE_DIRECTORY
 fi
 
 for file in ${SERVICE_FILES[@]}; do
-  if [ -e SYSTEMD_DIRECTORY/file]; then
+  if [ -e SYSTEMD_DIRECTORY/file ]; then
     rm -rf SYSTEMD_DIRECTORY/file
   fi
 done
