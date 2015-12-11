b32750126b92a38275b9c2a760435a511888a264
Bernardo Lins <bernardolins28@gmail.com>
2015-12-11 00:31:59 -0200

    Fix kubernetes environment file name

--- setup-config.sh
+++ setup-config.sh
@@ -2,7 +2,7 @@
 
 KUBE_DIRECTORY="/etc/kubernetes"
 SYSTEMD_DIRECTORY="/etc/systemd/system"
-
+ENVIRONMENT_FILE="kubernetes.env"
 SERVICE_FILES=("kube-api-server.service" "kube-controller-manager.service" "kube-proxy.service" "kube-scheduler.service" "kubelet.service")
 
 
@@ -10,7 +10,7 @@ if [ ! -d $KUBE_DIRECTORY ]; then
   mkdir -p $KUBE_DIRECTORY
 fi
 
-ln -s "$(pwd)/kubelet.env" "$KUBE_DIRECTORY/kubelet.env"
+ln -s "$(pwd)/$ENVIRONMENT_FILE" "$KUBE_DIRECTORY/$ENVIRONMENT_FILE"
 
 for file in ${SERVICE_FILES[@]}; do
   if [ ! -e "$SYSTEMD_DIRECTORY/$file" ]; then
