e985781145aa15ddf0ce8bf53e2673262f3a98dc
Bernardo Lins <bernardolins28@gmail.com>
2015-12-22 22:07:44 -0200

    Fix issue with mkdir and ln

--- setup-config.sh
+++ setup-config.sh
@@ -6,14 +6,28 @@ ENVIRONMENT_FILE="kubernetes.env"
 SERVICE_FILES=("kube-api-server.service" "kube-controller-manager.service" "kube-proxy.service" "kube-scheduler.service" "kubelet.service")
 
 
-if [ ! -d $KUBE_DIRECTORY ]; then
+function create_kube_directory {
+  echo "Creating kubernetes directory at $KUBE_DIRECTORY"
   mkdir -p $KUBE_DIRECTORY
-fi
+}
 
-ln -s "$(pwd)/$ENVIRONMENT_FILE" "$KUBE_DIRECTORY/$ENVIRONMENT_FILE"
-
-for file in ${SERVICE_FILES[@]}; do
-  if [ ! -e "$SYSTEMD_DIRECTORY/$file" ]; then
-    ln -s "$(pwd)/$file" $SYSTEMD_DIRECTORY
+function add_environment_file {
+  echo "Copying environment file to $KUBE_DIRECTORY/$ENVIRONMENT_FILE"
+  if [ ! -e "$KUBE_DIRECTORY/$ENVIRONMENT_FILE" ]; then
+    ln -s "$(pwd)/$ENVIRONMENT_FILE" "$KUBE_DIRECTORY/$ENVIRONMENT_FILE"
   fi
-done
+}
+
+function copy_systemd_unit_files {
+  for file in ${SERVICE_FILES[@]}; do
+    if [ ! -e "$SYSTEMD_DIRECTORY/$file" ]; then
+      echo "Copying $file to $SYSTEMD_DIRECTORY"
+      ln -s "$(pwd)/$file" "$SYSTEMD_DIRECTORY/$file"
+    fi
+  done
+}
+
+create_kube_directory
+add_environment_file
+copy_systemd_unit_files
+
